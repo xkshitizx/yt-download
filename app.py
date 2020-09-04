2a42f8d3c24c8300cea6be07068fbf587a4114ce
xkshitizx <kshitizlama03@gmail.com>
2020-09-04 19:20:23 +0545

    Final submission for mp3

---
+++ app.py
@@ -0,0 +1,32 @@
+import webbrowser
+
+from flask import Flask,render_template, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField
+
+from download import mp3_download
+
+
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SECRET_KEY']: str = os.getenv("APP_SECRET")
+
+
+class DownloadForm(FlaskForm):
+    """Form to take url as an input"""
+    url = StringField('Url')
+
+
+
+@app.route('/',methods=['GET', 'POST'])
+def index():
+    form = DownloadForm()
+
+    if form.validate_on_submit():
+        return render_template("downloading.html", url=form.url.data, func=mp3_download)
+
+    return render_template("index.html", form=form)
+
+if __name__ == '__main__':
+    app.run()
---
+++ download.py
@@ -0,0 +1,36 @@
+import youtube_dl
+import getpass
+
+def mp3_download(url):
+    """
+    Takes in youtube url and downloads mp3
+
+    Parameter:url(string)
+    """
+    try:
+    #Ask the user for the video they want to download
+        path_to_download_folder = f"/home/{getpass.getuser()}/Downloads"
+        #Download and convert the file into mp3 and store in downloads folder
+        video_info = youtube_dl.YoutubeDL().extract_info(
+            url=url, download=False
+        )
+        
+        filename = f"{video_info['title']}.mp3"
+        if (len(filename)>34):
+            filename = filename[0:30]+'.mp3'
+        options = {
+            'format': 'bestaudio/best',
+            'keepvideo': False,
+            'outtmpl': f'/home/kshitiz/Downloads/{filename}',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality':'192',
+                }]
+            }
+
+        with youtube_dl.YoutubeDL(options) as ydl:
+            ydl.download([video_info['webpage_url']])
+        return 0
+    except:
+        return 1
